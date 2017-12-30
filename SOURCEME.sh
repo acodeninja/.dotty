f4d17a708b1bac7a7625c9b62f7b069296993917
Lawrence <lawrence@acode.ninja>
2017-12-30 14:37:57 +0000

    Adds bin directory with ignores

--- SOURCEME.sh
+++ SOURCEME.sh
@@ -3,6 +3,9 @@
 # Get the current directory
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Add dotty bin to path
+export PATH="$PATH:$THIS_DIR/bin"
+
 # Load modules
 for MODULE in `find $THIS_DIR/modules/* -type d`
 do
