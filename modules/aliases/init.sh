efb6ed5b4e209a240c640345a1fcb7378e531a8b
Lawrence <lawrence@acode.ninja>
2017-12-30 15:08:52 +0000

    adds alias scripts

---
+++ modules/aliases/init.sh
@@ -0,0 +1,17 @@
+## Colorize the grep command output for ease of use (good for log files)##
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# do not delete / or prompt if deleting more than 3 files at a time #
+alias rm='rm -I --preserve-root'
+
+# confirmation #
+alias mv='mv -i'
+alias cp='cp -i'
+alias ln='ln -i'
+
+# parenting changing perms on / #
+alias chown='chown --preserve-root'
+alias chmod='chmod --preserve-root'
+alias chgrp='chgrp --preserve-root'
